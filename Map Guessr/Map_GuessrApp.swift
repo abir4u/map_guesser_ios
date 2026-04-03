6da9ab5542d23d803dc63a400030e9e239ffa1dc
abir4u <abirpal90@gmail.com>
2026-04-03 13:45:35 +1300

    Create iOS app for Map Guessr

---
+++ Map Guessr/Map_GuessrApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_GuessrApp.swift
+//  Map Guessr
+//
+//  Created by Abir Pal on 03/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Map_GuessrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Map GuessrTests/Map_GuessrTests.swift
@@ -0,0 +1,16 @@
+//
+//  Map_GuessrTests.swift
+//  Map GuessrTests
+//
+//  Created by Abir Pal on 03/04/2026.
+//
+
+import Testing
+
+struct Map_GuessrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
