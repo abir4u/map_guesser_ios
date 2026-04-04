9e1fd908fef131a0474904c0785dac2fd8d3b5a7
abir4u <abirpal90@gmail.com>
2026-04-04 21:38:05 +1300

    Create extension to globally handle keyboard dismissal

---
+++ Map Guessr/Core/Extensions/View+Keyboard.swift
@@ -0,0 +1,28 @@
+//
+//  View+Keyboard.swift
+//  Map Guessr
+//
+//  Created by Abir Pal on 04/04/2026.
+//
+
+import SwiftUI
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
+    func dismissKeyboardOnTap() -> some View {
+        return self.modifier(DismissKeyboardModifier())
+    }
+}
+
+struct DismissKeyboardModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .contentShape(Rectangle())
+            .onTapGesture {
+                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+            }
+    }
+}
