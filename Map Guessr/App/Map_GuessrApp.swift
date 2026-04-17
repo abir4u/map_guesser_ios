94766c815967f0636b9fbbce92aacdb3e32253e9
abir4u <abirpal90@gmail.com>
2026-04-18 06:55:11 +1200

    Modify Map_GuessrApp to check if it is running in a test environment before it runs any setup code or initializes views. When XCTest launches, it should not try to configure the real GIDSignIn.sharedInstance via app's init, while unit tests simultaneously try to use or interact with that same singleton.

--- Map Guessr/App/Map_GuessrApp.swift
+++ Map Guessr/App/Map_GuessrApp.swift
@@ -11,8 +11,19 @@ import GoogleSignIn
 @main
 struct Map_GuessrApp: App {
     init() {
-        let config = GIDConfiguration(clientID: "298305099236-e5lv6njj1tvav79jj27qj8qhg5eonfct.apps.googleusercontent.com")
-        GIDSignIn.sharedInstance.configuration = config
-    }
-    var body: some Scene { WindowGroup { HomeView() } }
+            if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] == nil {
+                let config = GIDConfiguration(clientID: "298305099236-e5lv6njj1tvav79jj27qj8qhg5eonfct.apps.googleusercontent.com")
+                GIDSignIn.sharedInstance.configuration = config
+            }
+        }
+        
+        var body: some Scene {
+            WindowGroup {
+                if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] == nil {
+                    HomeView()
+                } else {
+                    Text("Running Unit Tests...")
+                }
+            }
+        }
 }
