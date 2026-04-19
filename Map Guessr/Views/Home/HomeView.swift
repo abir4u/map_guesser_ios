637783cbf0449a2da720e245e4fa1b455f154ff0
abir4u <abirpal90@gmail.com>
2026-04-19 17:02:00 +1200

    Modularise HomeViewButton

--- Map Guessr/Views/Home/HomeView.swift
+++ Map Guessr/Views/Home/HomeView.swift
@@ -126,45 +126,6 @@ struct HomeView: View {
     }
 }
 
-struct HomeMenuButton: View {
-    let title: String
-    let icon: String
-    let topColor: Color
-    let bottomColor: Color
-    let action: () -> Void
-    
-    var body: some View {
-        Button(action: action) {
-            HStack {
-                Image(systemName: icon)
-                    .font(.title2)
-                Text(title)
-                    .font(.title3.bold())
-                Spacer()
-                Image(systemName: "play.fill")
-                    .font(.caption)
-            }
-            .padding(.vertical, 18)
-            .padding(.horizontal, 25)
-            .frame(maxWidth: .infinity)
-            .background(
-                LinearGradient(
-                    stops: [
-                        .init(color: topColor, location: 0.5),
-                        .init(color: bottomColor.opacity(0.6), location: 1.2)
-                    ],
-                    startPoint: .top,
-                    endPoint: .bottom
-                )
-            )
-            .foregroundColor(.white)
-            .cornerRadius(20)
-            .shadow(color: topColor.opacity(0.4), radius: 10, x: 0, y: 5)
-        }
-        .buttonStyle(GameButtonStyle())
-    }
-}
-
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
