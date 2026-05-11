3b2d989186e8b13c2c63c1fa2bca7242d764cc37
abir4u <abirpal90@gmail.com>
2026-05-11 19:28:32 +1200

    Create LaunchService to integrate the new API on launch

---
+++ Map Guessr/Services/LaunchService.swift
@@ -0,0 +1,41 @@
+//
+//  FeatureFlags.swift
+//  Map Guessr
+//
+//  Created by Abir Pal on 11/05/2026.
+//
+
+import Foundation
+internal import Combine
+
+struct FeatureFlags: Decodable {
+    let authenticate_soloplay: Bool
+    let multiplayer_mode: Bool
+}
+
+class LaunchService: ObservableObject {
+    @Published var flags: FeatureFlags?
+    
+    private let defaultFlags = FeatureFlags(
+        authenticate_soloplay: false,
+        multiplayer_mode: false,
+    )
+
+    @MainActor
+    func fetchFeatureFlags() async {
+        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0.0"
+        let environment = AppConfig.environmentName
+        
+        let urlString = "\(AppConfig.Endpoints.features)?version=\(version)&environment=\(environment)"
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        do {
+            let fetchedFlags: FeatureFlags = try await NetworkClient.request(url)
+            self.flags = fetchedFlags
+        } catch {
+            print("Failed to fetch flags: \(error). Using defaults.")
+            self.flags = self.defaultFlags
+        }
+    }
+}
